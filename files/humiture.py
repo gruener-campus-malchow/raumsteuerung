ca3be3c51dcd87e08af05d33645074025f181247
HansSarpeiDerKing <d.ebner13@web.de>
2017-11-22 11:46:35 +0100

    Add files via upload

---
+++ files/humiture.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# author = HansSarpeiDerKing
+from sensor import Sensor 
+
+class Humiture(Sensor)
+
+	def __init__(self):
+		self._temperature = []
+		self._humiture = []
+
+	def getTemperature(self):
+		return self._temperatur
+	
+
+	def getHumiture(self):
+		return self._humiture
+
+	def test(self).
+			print 'test Ergebnisse getResult():'+self.getResult()
+
+		
