27ee7d2641c937a928399fa9e672b430b07c15d3
ijontychie <baldauf@gruener-campus-malchow.de>
2017-11-22 12:12:22 +0100

    Create first Tests for sound an pumpe

---
+++ files/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from sound import Sound
+from pumpe import Pumpe
+
+
+soundModul= Sound('testSoundModul')
+soundModul.test()
+
+pumpModul = Pumpe('testPumpe')
+pumpModul.test()
