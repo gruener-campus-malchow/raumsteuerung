3e6383e165f5b280e545a35e7f386ec39441560d
ijontychie <baldauf@gruener-campus-malchow.de>
2017-11-22 12:23:04 +0100

    comment aout pumpe

--- files/test.py
+++ files/test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 from sound import Sound
-from pumpe import Pumpe
+#from pumpe import Pumpe
 from buzzer import Buzzer
-
+add utf-8 capability to all classes
 
 soundModul= Sound('testSoundModul')
 soundModul.test()
