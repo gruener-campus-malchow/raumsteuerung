c3c4747367d327f63b42598f086ec816bf9f7de0
ZoccaX <jan.aulich@gmx.de>
2017-11-25 08:52:59 +0100

    test function in class Pumpe outputs 100 test values

--- files/pumpe.py
+++ files/pumpe.py
@@ -17,11 +17,12 @@ class Pumpe(Aktor):
 
 	def test(self):
 
-		randStat = random.randrange(0, 1000)
+		for i in range(1, 100):
 
-		print('Test-Start gesetzte Wassermenge mit setWassermenge():'+ str(randStat) + ' L/s')
-		self.setWassermenge(randStat)
-		print('Test-Ergebnisse getWassermenge():'+ str(self.getWassermenge()) + ' L/s')
+			randStat = random.randrange(0, 1000)
+			print('Test-Start gesetzte Wassermenge mit setWassermenge():'+ str(randStat) + ' L/s')
+			self.setWassermenge(randStat)
+			print('Test-Ergebnisse getWassermenge():'+ str(self.getWassermenge()) + ' L/s')
 
 testPumpe = Pumpe()
 testPumpe.test()
